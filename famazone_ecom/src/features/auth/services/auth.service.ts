842c62c1f34ecb58baf8371ede4e1c6714fb16c8
Elham-EN <ilhamrezaie@gmail.com>
2023-06-04 19:22:09 +1000

    JSDoc in the Auth Service for the Auth Feature

--- famazone_ecom/src/features/auth/services/auth.service.ts
+++ famazone_ecom/src/features/auth/services/auth.service.ts
@@ -7,17 +7,55 @@ import { JWT } from "../models/Jwt";
 import { DecodedJwt } from "../models/DecodedJwt.interface";
 
 const baseURL = process.env.REACT_APP_BASE_API as string;
+type DisplayUserType = DisplayUser | null;
 
-const register = async (newUser: NewUser): Promise<DisplayUser | null> => {
+/**
+ * This function registers a new user.
+ *
+ * It sends a POST request to the '/auth/register' endpoint with the new user's
+ * information as the payload. The server is expected to create a new user with
+ * the provided information and return a `DisplayUserType` object representing
+ * the newly created user.
+ *
+ * @param {NewUser} newUser - An object representing the new user's information.
+ *
+ * @returns {Promise<DisplayUserType>} A promise that resolves to an object
+ * representing the newly created user.
+ *
+ */
+async function register(newUser: NewUser): Promise<DisplayUserType> {
   const response = await axios.post(`${baseURL}/auth/register`, newUser);
   return response.data;
-};
+}
 
-// In authentication, when the user successfully logs in using their
-// credentials, a JSON Web Token will be returned.
-const login = async (
-  user: LoginUser
-): Promise<{ jwt: JWT; user: DisplayUser | null }> => {
+interface JwtAndUserType {
+  jwt: JWT;
+  user: DisplayUserType;
+}
+/**
+ * This function logs in a user using their credentials, which should be encapsulated
+ * in a `LoginUser` object.
+ *
+ * It sends a POST request to the '/auth/login' endpoint with the user credentials as
+ * the payload. The server is expected to respond with a JSON object containing a JWT
+ * The function then decodes the JWT to extract the user information, stores the JWT
+ * and the user information in the local storage, and returns the JWT and the user
+ * information.
+ *
+ * @param {LoginUser} user - An object representing a user's credentials (username
+ * and password).
+ *
+ * @returns {Promise<{ jwt: JWT; user: DisplayUser | null }>} A promise that resolves
+ * to an object containing the JWT and the user information extracted from the JWT.
+ * If the server response does not contain a valid JWT, the promise resolves to an
+ * object with `jwt` being the server response and `user` being `null`.
+ *
+ * @remarks
+ * The data stored in the local storage will persist even after closing the browser,
+ * providing a method to keep users logged in across multiple browsing sessions.
+ *
+ */
+async function login(user: LoginUser): Promise<JwtAndUserType> {
   const response = await axios.post(`${baseURL}/auth/login`, user);
   // response' data must include JSON object: { "token": "jwt token" }
   if (response.data) {
@@ -30,25 +68,42 @@ const login = async (
     return { jwt: response.data, user: decodedJwt.user };
   }
   return { jwt: response.data, user: null };
-};
+}
 
-const logout = (): void => {
+/**
+ * Remove user and jwt object from the browser'local storage (Sign Out)
+ */
+function logout(): void {
   localStorage.removeItem("user");
   localStorage.removeItem("jwt");
-};
+}
 
-const verifyJwt = async (jwt: string): Promise<boolean> => {
+/**
+ * This function verifies the validity of a JWT (JSON Web Token).
+ *
+ * It sends a POST request to the '/auth/verify-jwt' endpoint with the JWT as the
+ * payload. The server is expected to respond with a payload containing the expiration
+ * timestamp of the JWT. The function then compares this expiration timestamp with the
+ * current time to determine whether the JWT is still valid.
+ *
+ * @param {string} jwt - The JSON Web Token (JWT) to be verified
+ *
+ * @returns {Promise<boolean>} if the expiration of the JWT is greater than the current
+ * timestamp at the time of verification
+ *
+ * @remarks
+ * This function is part of a system designed to mitigate the risk of using outdated or
+ * expired tokens for authentication or authorization purposes. An invalid token could
+ * be due to it being tampered with, expired, or issued by an unauthorized entity.
+ */
+async function verifyJwt(jwt: string): Promise<boolean> {
   const response = await axios.post(`${baseURL}/auth/verify-jwt`, { jwt });
   if (response.data) {
-    // In order to verify JWT, the expiration of JWT must be greater then
-    // the current timestamp at the time of verification.  to mitigate the
-    // risk of using outdated or expired tokens for authentication or
-    //authorization purposes
     const jwtExpirationMs = response.data.exp * 1000;
     return jwtExpirationMs > Date.now();
   }
   return false; // User is not validated
-};
+}
 
 const authService = {
   register,
